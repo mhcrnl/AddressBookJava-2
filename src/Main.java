7d7548ba979e190753c2764e87a82102cce71dd9
irinaiarca <iarcairina@gmail.com>
2014-10-15 22:36:44 +0300

    Fixed init issue, added some stuff.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,32 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main 
 {
-	public static void main()
+	public static void main(String[] arg)
 	{
-		Persoana a;
+		Persoana a = new Persoana("Nume", "Prenume", "Email", "Telefon");
 		String x;
 		System.out.println("Se citeste o persoana:");
 		Scanner user_input = new Scanner(System.in); //"Resource leak: 'user_input' is never closed"
-		x=user_input.next();
+		
+		System.out.println("Nume: ");
+		x = user_input.next();
 		a.setName(x); //"Local variable may not have been initialized"
+		
+		System.out.println("Prenume: ");
+		x = user_input.next();
+		a.setSurname(x);
+		
+		System.out.println("E-mail: ");
+		x = user_input.next();
+		a.setEmail(x);
+		
+		System.out.println("Telefon: ");
+		x = user_input.next();
+		a.setPhone(x);
+		
+		System.out.println("Cum printez un string?");
+		
 	}
 }
